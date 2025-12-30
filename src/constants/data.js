a1f0858d75d8e2c7452560eaf25a05e42e6eae1b
aditya sangale <sangleaditya824@gmail.com>
2025-12-30 15:17:04 +0530

    Fixed empty data.js file

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,12 +1,10 @@
 
-import React from 'react';
-
 export const CONTACT_INFO = {
     name: "Aditya Sangale",
     title: "Frontend Developer",
     email: "sangleaditya824@gmail.com",
     phone: "+91 82628 15229",
-    phoneRaw: "918262815229", 
+    phoneRaw: "918262815229",
     location: "Nashik, India",
     socials: {
         github: "https://github.com/aditya-sangale",
